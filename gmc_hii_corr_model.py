e33fc4aab9fc5393ef0c53485fe91d23951494d7
Guillermo Blanc <gblancm@carnegiescience.edu>
2020-10-30 12:08:42 -0300

    updated gmc_hii_corr_model.py

--- gmc_hii_corr_model.py
+++ gmc_hii_corr_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[49]:
 
 
 import numpy as np
@@ -16,7 +16,7 @@ from scipy.optimize import curve_fit
 
 # ## Name of galaxy
 
-# In[2]:
+# In[50]:
 
 
 #galaxy='IC5332'
@@ -33,7 +33,7 @@ galaxy='NGC3627'
 
 # ## Function that calculates the 2-point cross-correlation function
 
-# In[3]:
+# In[133]:
 
 
 def w(x1, y1, x2, y2, xr, yr, rmin=0, rmax=5000, dr=25):
@@ -51,21 +51,25 @@ def w(x1, y1, x2, y2, xr, yr, rmin=0, rmax=5000, dr=25):
     dr0, dr0bins = np.histogram(drarr, bins=np.arange(rmin, rmax+dr, dr))
     
     # Normalize pair counts and compute cross-correlation function
-    dd=dd0/N1/N2
-    dr=dr0/N1/Nr
-    omega=dd/dr-1
+    if (N1!=0)*(N2!=0)*(Nr!=0):
+        dd=dd0/N1/N2
+        dr=dr0/N1/Nr
+        omega=dd/dr-1
     
-    edd=np.sqrt(dd0)/N1/N2
-    edr=np.sqrt(dr0)/N1/Nr
+        edd=np.sqrt(dd0)/N1/N2
+        edr=np.sqrt(dr0)/N1/Nr
 
-    eomega=np.sqrt((edd/dr)**2+(dd*edr/dr**2)**2)
-
+        eomega=np.sqrt((edd/dr)**2+(dd*edr/dr**2)**2)
+    else:
+        omega=np.repeat(np.nan, len(bins))
+        eomega=np.repeat(np.nan, len(bins))
+        
     return (bins, omega, eomega)
 
 
 # ## Functions that draws N model GMCs and returns properties
 
-# In[4]:
+# In[134]:
 
 
 # This version just uses the observed GMC coordinates and assumes constant parameters for all clouds
@@ -92,10 +96,10 @@ def drawgmc_xy(x,y,rc=25,tc=30,ts=10,tfb=5,Ng=1,voff=10):
     
 
 
-# In[5]:
+# In[135]:
 
 
-# This version samples GMC coordinates with a typical separation scale "l" within a 2x2 kpc box, and assumes constant parameters for all clouds
+# This version samples GMC coordinates with a typical separation scale "l" within a dbox**2 kpc**2 box, and assumes constant parameters for all clouds
 # Also returns random catalog coordinates over the same area and Frand*Ngmc points
 def drawgmc_l(dbox=2000,l=200,rc=25,tc=30,ts=10,tfb=5,Ng=1,voff=10, frand=10):
 
@@ -108,7 +112,6 @@ def drawgmc_l(dbox=2000,l=200,rc=25,tc=30,ts=10,tfb=5,Ng=1,voff=10, frand=10):
     xr=np.random.uniform(-0.5*dbox, 0.5*dbox, frand*ngmc) # gmc x coordinates [pc]
     yr=np.random.uniform(-0.5*dbox, 0.5*dbox, frand*ngmc) # gmc y coordinates [pc]
 
-    
     rc=np.repeat(rc,ngmc) # gmc radius [pc]
     tc=np.repeat(tc,ngmc) # gmc lifetime [Myr]
     ts=np.repeat(ts,ngmc) # stellar SF tracer lifetime [Myr]
@@ -120,14 +123,14 @@ def drawgmc_l(dbox=2000,l=200,rc=25,tc=30,ts=10,tfb=5,Ng=1,voff=10, frand=10):
     fgmc=np.repeat(True, ngmc) # GMC visibility flag
     fgmc[tobs > tc]=False # visibility flag is False if GMC has faded
 
-    print("Generating GMC and random coordinates:", ngmc, frand*ngmc)
+    #print("Generating GMC and random coordinates:", ngmc, frand*ngmc)
     return (xgmc, ygmc, rc, tc, ts, tfb, Ng, voff, tobs, fgmc, xr, yr)
     
 
 
 # ## Function that draws N*Ng HII regions given an ensemble of GMCs
 
-# In[6]:
+# In[136]:
 
 
 def drawhii(xgmc, ygmc, rc, tc, ts, tfb, Ng, voff, tobs, fgmc):
@@ -136,16 +139,14 @@ def drawhii(xgmc, ygmc, rc, tc, ts, tfb, Ng, voff, tobs, fgmc):
     nhii=np.sum(Ng.astype(int)) # number of HII regions
     voffaux=voff*3.2e-14*(1e6*365*24*3600) # velocity in pc/Myr
 
-
     xhii=np.zeros(nhii) # HII region initial coordinates
     yhii=np.zeros(nhii)
     t0=np.zeros(nhii) # HII region formation time
     fhii=np.repeat(False,nhii) # HII region visibility flag
     indgmc=np.zeros(nhii) # gmc list index
 
-    
     k=0 # counter
-    for i in range(ngmc):
+    for i in range(ngmc):  # drawing HII regions for each GMC independently
         for j in range(Ng[i]):
 
             # initial position
@@ -155,7 +156,7 @@ def drawhii(xgmc, ygmc, rc, tc, ts, tfb, Ng, voff, tobs, fgmc):
             x0=xgmc[i]+r0*np.cos(theta0)
             y0=ygmc[i]+r0*np.sin(theta0)
 
-            #formation time
+            #formation time (assuming stars form no later than tc-tfb)
             t0[k]=np.random.uniform(0, tc[i]-tfb[i])    
             
             #offset direction and final position at tobs
@@ -183,7 +184,7 @@ def drawhii(xgmc, ygmc, rc, tc, ts, tfb, Ng, voff, tobs, fgmc):
 
 # ## Read GMC and Random catalogs coordinates
 
-# In[7]:
+# In[137]:
 
 
 xygmc=ascii.read('./output/'+galaxy+'_xy_gmc.txt')
@@ -197,7 +198,7 @@ yr=xyrand['col1'].data
 
 # ## Read Observed Correlation Function
 
-# In[8]:
+# In[138]:
 
 
 obscorr=ascii.read('./output/'+galaxy+'_corr.txt')
@@ -206,39 +207,55 @@ w0obs=obscorr['col1'].data
 ew0obs=obscorr['col2'].data
 
 
+# In[139]:
+
+
+# NEXT I WILL RUN AN MCMC TEST IN MY LAPTOP TURNING THE NEXT CELL INTO A FUNCTION
+
+
 # ## Test Model
 
-# In[20]:
+# In[187]:
 
 
 t0=time.time()
 
-fig, ax = plt.subplots(figsize=(12, 8))
+Nsamples=200
 
-for i in range(9):
+# Run one time with small box to get bins
+xgmc, ygmc, rc, tc, ts, tfb, Ng, voff, tobs, fgmc, xr, yr = drawgmc_l(dbox=2000, l=300, rc=50,tc=20,ts=15,tfb=4,Ng=10,voff=10,frand=10)
+xhii, yhii, fhii = drawhii(xgmc, ygmc, rc, tc, ts, tfb, Ng, voff, tobs, fgmc)
+r0, w0, ew0 = w(xgmc[fgmc], ygmc[fgmc], xhii[fhii], yhii[fhii], xr, yr, rmax=1000)
 
-#    xgmc, ygmc, rc, tc, ts, tfb, Ng, voff, tobs, fgmc = drawgmc_xy(x1, y1, rc=20,tc=100,ts=10,tfb=4,Ng=1,voff=10)
-    IC=0.2
-    xgmc, ygmc, rc, tc, ts, tfb, Ng, voff, tobs, fgmc, xr, yr = drawgmc_l(dbox=10000, l=300, rc=50,tc=20,ts=15,tfb=4,Ng=10,voff=10,frand=10)
+w0arr=np.zeros((len(w0),Nsamples))
+ew0arr=np.zeros((len(ew0),Nsamples))
+
+for i in range(Nsamples):
+    
+    xgmc, ygmc, rc, tc, ts, tfb, Ng, voff, tobs, fgmc, xr, yr = drawgmc_l(dbox=3000, l=300, rc=20,tc=30,ts=10,tfb=4,Ng=3,voff=10,frand=10)
     xhii, yhii, fhii = drawhii(xgmc, ygmc, rc, tc, ts, tfb, Ng, voff, tobs, fgmc)
-    r0, w0, ew0 = w(xgmc[fgmc], ygmc[fgmc], xhii[fhii], yhii[fhii], xr, yr, rmax=1000)
-    w0=w0+IC
+    r0, w0arr[:,i], ew0arr[:,i] = w(xgmc[fgmc], ygmc[fgmc], xhii[fhii], yhii[fhii], xr, yr, rmax=1000)
+        
+IC=0.2
+w0arr=w0arr+IC
+w0arr[~np.isfinite(w0arr)]=np.nan
+ew0arr[~np.isfinite(ew0arr)]=np.nan
+        
+w0=np.nanmean(w0arr, axis=1)
+ew0=np.nanmean(ew0arr, axis=1)/np.sqrt(Nsamples)
     
 
     
-
-#    print(w0)
-    
-#    w0=w0/12
-#    ew0=ew0/12
-    
-    #ax.errorbar(r0, w0, ew0, fmt="-o", color='red', capsize=5, alpha=0.5)
-    ax.plot(r0, w0, 'o', color='red', alpha=0.5)
-    print("Run Time [s] =", time.time()-t0)
-
 print("Total Run Time [s] =", time.time()-t0)
 
 
+  
+fig, ax = plt.subplots(figsize=(12, 8))
+#for i in range(Nsamples):
+#    ax.plot(r0, w0arr[:,i], 'o', color='red', alpha=0.5)
+#for i in range(Ntest):
+#    ax.plot(r0, w0test[:,i], 'o', color='green', alpha=0.5)
+ax.plot(r0, w0, 'o', color='red', alpha=1.0)    
 ax.errorbar(r0obs, w0obs, ew0obs, fmt="o", color='black', capsize=5, alpha=0.5)
 ax.plot(r0obs, w0obs, '-o', color='black', alpha=0.5)
 ax.set_xlim(0, 1000)
@@ -248,8 +265,14 @@ ax.set_ylabel(r'$\omega(r)$ [pc]', fontsize=20)
 ax.set_title(galaxy, fontsize=30)
 ax.tick_params(labelsize=20)
 #ax.set_yscale('log')
+#ax.set_ylim(-0.2, 4)
 plt.savefig('./plots/'+galaxy+'_corr_model.png')
 #plt.show()
+
+
+
+# In[174]:
+
 
 
 fig, ax = plt.subplots(figsize=(12, 12))
@@ -257,8 +280,8 @@ fig, ax = plt.subplots(figsize=(12, 12))
 ax.plot(xgmc[fgmc], ygmc[fgmc], 'o', color='blue', label='GMC')
 #ax.plot(xhii, yhii, '.', color='red', alpha=0.4)
 ax.plot(xhii[fhii], yhii[fhii], 'o', color='red', label='HII')
-ax.set_xlim(-1000, 1000)
-ax.set_ylim(-1000, 1000)
+#ax.set_xlim(-1000, 1000)
+#ax.set_ylim(-1000, 1000)
 ax.set_xlabel('X [pc]', fontsize=20)
 ax.set_ylabel('Y [pc]', fontsize=20)
 ax.set_title(galaxy, fontsize=30)
@@ -311,6 +334,15 @@ ax.tick_params(labelsize=20)
 ax.set_yscale('log')
 plt.savefig('./plots/'+galaxy+'_corr_log_model.png')
 #plt.show()
+
+
+# In[81]:
+
+
+
+
+a=np.array([[2,2],[3,3],[4,4]])
+np.mean(a, axis=1)
 
 
 # In[ ]:
