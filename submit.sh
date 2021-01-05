ce4308a515b6eafb2cd07640208a6ea83dd04a89
Guillermo Blanc <gblancm@carnegiescience.edu>
2021-01-05 16:48:33 -0300

    fixed submit.sh

--- submit.sh
+++ submit.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #
 #SBATCH --job-name=gmc_hii_corr_mod
-#SBATCH --output=output.txt
-#
-#SBATCH --ntasks=5
-#SBATCH --time=10:00
-#SBATCH --mem-per-cpu=100
+#SBATCH --nodes=1 
+#SBATCH --ntasks-per-node=24  
+#SBATCH --time=24:00:00     
+#SBATCH -p OBS,SHARED
+#SBATCH --mem-per-cpu=2000
+#SBATCH --output=slurm-${SLURM_JOBID}.out 
+#SBATCH --error=slurm-${SLURM_JOBID}.out 
+#SBATCH --mail-user=gblancm@carnegiescience.edu 
+#SBATCH --mail-type=END,FAIL 
 
-srun module load python
+#srun module load python
 srun python gmc_hii_corr_model.py 
