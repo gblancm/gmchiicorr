102dacb8ed4ecb5a098ce8530498d8c1ab3e21dd
Guillermo Blanc <gblancm@memex.carnegiescience.edu>
2021-01-14 11:23:06 -0800

    commit from memex

---
+++ NGC0628_submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+#SBATCH --job-name=gmc_hii_corr_mod
+#SBATCH --nodes=1 
+#SBATCH --ntasks=1
+#SBATCH --ntasks-per-node=12 
+#SBATCH --time=24:00:00     
+#SBATCH -p OBS,SHARED
+#SBATCH --mem-per-cpu=4000
+#SBATCH --output=output.out 
+#SBATCH --error=error.out 
+#SBATCH --mail-user=gblancm@carnegiescience.edu 
+#SBATCH --mail-type=END,FAIL 
+
+#srun module load python
+srun python gmc_hii_corr_model.py NGC0628
