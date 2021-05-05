798629c41f9eef1786dbbddfdb982f25f34e7c78
Guillermo Blanc <gblancm@carnegiescience.edu>
2021-05-05 16:15:00 -0300

    updated sbatch scripts

--- IC5332_submit.sh
+++ IC5332_submit.sh
@@ -4,12 +4,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=48:00:00
-#SBATCH -p OBS,SHARED
+#SBATCH --time=24:00:00
+#SBATCH -p OBS
 #SBATCH --mem-per-cpu=4000
 #SBATCH --output=IC5332_output.out
 #SBATCH --error=IC5332_error.out
 #SBATCH --mail-user=gblancm@carnegiescience.edu
-#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-type=BEGIN,END,FAIL
 
 srun python fitmodel.py IC5332
