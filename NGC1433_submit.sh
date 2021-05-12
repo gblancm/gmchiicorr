9697d9e16fb29663d225cb6bc238bda7a7c2dcef
Guillermo Blanc <gblancm@carnegiescience.edu>
2021-05-12 16:43:57 -0400

    update sbatch scripts

--- NGC1433_submit.sh
+++ NGC1433_submit.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=12:00:00
+#SBATCH --time=18:00:00
 #SBATCH -p OBS,PREEMPTION,SHARED
 #SBATCH --mem-per-cpu=1000
 #SBATCH --output=NGC1433_output.out
