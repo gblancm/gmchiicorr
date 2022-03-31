2b61eed5b193aae0a2604bcb49dc3dca37732ae2
Guillermo Blanc <gblancm@carnegiescience.edu>
2022-03-31 15:01:47 -0300

    limit curve_fit to r<200 pc

--- fitmodel.py
+++ fitmodel.py
@@ -102,7 +102,9 @@ p0=np.array([200, 100, 50, 2, 10])
 auxr=np.concatenate([r0obs,np.array([-1])])
 auxw=np.concatenate([w0obs,np.array([fhgobs])])
 auxew=np.concatenate([ew0obs,np.array([efhgobs])])
-pstart, pcov = curve_fit(func1, auxr, auxw, p0=p0, sigma=auxew, method='lm', epsfcn=0.01)
+# Limit initial guess fit to r<200 pc
+auxsel=(auxr<200)
+pstart, pcov = curve_fit(func1, auxr[auxsel], auxw[auxsel], p0=p0, sigma=auxew[auxsel], method='lm', epsfcn=0.01)
 pstart=np.array([pstart[0], pstart[1], pstart[2], tsprior, pstart[3], Ngprior, pstart[4]])
 
 
